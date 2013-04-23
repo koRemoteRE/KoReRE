4c9aef449d9111d76c76decb56928765ed686056
Thomas Kipshagen <tkipshagen@uni-koblenz.de>
2013-04-23 11:48:56 +0200

    Client

---
+++ ReRe-Client/ReRe-Client/PreRendering.cpp
@@ -0,0 +1,9 @@
+//
+//  PreRendering.cpp
+//  ReRe-Client
+//
+//  Created by Thomas Kipshagen on 23.04.13.
+//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
+//
+
+#include "PreRendering.h"
--- ReRe-Client/ReRe-Client/main.cpp
+++ ReRe-Client/ReRe-Client/main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[])
 {
-
+    
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
