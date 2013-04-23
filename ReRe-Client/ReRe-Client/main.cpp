70c79b3530a08d9dd94866e1c8c622821aa5dc14
Thomas Kipshagen <tkipshagen@uni-koblenz.de>
2013-04-23 11:43:12 +0200

    Client

---
+++ ReRe-Client/ReRe-Client/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ReRe-Client
+//
+//  Created by Thomas Kipshagen on 23.04.13.
+//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
