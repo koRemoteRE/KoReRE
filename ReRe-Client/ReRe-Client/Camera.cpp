5fd3f1c428bba2d2b79da5353a21f3f8038ff177
Ina Schröder <inaschroder@Ina-Schroders-MacBook-Pro.local>
2013-04-23 12:11:51 +0200

    Camera update

---
+++ ReRe-Client/ReRe-Client/Camera.cpp
@@ -0,0 +1,11 @@
+//
+//  Camera.cpp
+//  ReRe-Client
+//
+//  Created by Ina Schröder on 23.04.13.
+//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
+//
+
+#include "Camera.h"
+
+
---
+++ ReRe-Client/ReRe-Client/Camera.h
@@ -0,0 +1,14 @@
+//
+//  Camera.h
+//  ReRe-Client
+//
+//  Created by Ina Schröder on 23.04.13.
+//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
+//
+
+#ifndef __ReRe_Client__Camera__
+#define __ReRe_Client__Camera__
+
+#include <iostream>
+
+#endif /* defined(__ReRe_Client__Camera__) */
