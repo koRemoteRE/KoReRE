f5424217a10ed8fa42908e338f6db7a0c2a0c81c
Martina Sekulla <msekulla@uni-koblenz.de>
2013-05-05 22:28:48 +0200

    Client - Update: CMain

--- ReRe-Client/ReRe-Client/CMain.cpp
+++ ReRe-Client/ReRe-Client/CMain.cpp
@@ -30,8 +30,8 @@ int main(int argc, const char * argv[])
         }
         
         glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4); // 4x antialiasing
-        glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3); // OpenGL 3.3
-        glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+        glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3); // OpenGL 3.2
+        glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
         glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
         
         // Open a window and create its OpenGL context
