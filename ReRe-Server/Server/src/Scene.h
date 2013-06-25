6c1fdbac4cc6fbe1d4e2bd9ef2454f5393822f5e
Andreas Weinmann <andy.weinmann@gmail.com>
2013-06-25 17:29:54 +0200

    missing files

---
+++ ReRe-Server/Server/src/Scene.h
@@ -0,0 +1,29 @@
+#ifndef SERVER_SCENE_H
+#define SERVER_SCENE_H
+#include "KoRE/Common.h"
+#include "KoRE/Operations/Operations.h"
+#include "KoRE/SceneNode.h"
+#include "KoRE/Passes/ShaderProgramPass.h"
+#include "KoRE/Components/LightComponent.h"
+#include "KoRE/GLerror.h"
+#include "KoRE/Components/TexturesComponent.h"
+#include "KoRE/ResourceManager.h"
+
+
+class Scene
+{
+public:
+  Scene(void);
+  ~Scene(void);
+  void init();
+  void update(double time);
+  inline kore::Camera* getCam(){return pCamera;}
+  
+private:
+  void setUpSimpleRendering(kore::SceneNode* renderNode, kore::ShaderProgramPass* programPass, kore::Texture* texture, kore::LightComponent* light);
+  void setUpNMRendering(kore::SceneNode* renderNode, kore::ShaderProgramPass* programPass, kore::Texture* texture, kore::Texture* normalmap, kore::LightComponent* light);
+  kore::SceneNode* rotationNode;
+  kore::SceneNode* lightNode;
+  kore::Camera* pCamera;
+};
+#endif //SERVER_SCENE_H
