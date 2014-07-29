db9312d53e51babca3db2ec2a0da492dbdec0ca0
ThomasKipshagen <tkipshagen@uni-koblenz.de>
2014-07-29 11:22:18 +0200

    Update CSceneNode.h

--- ReRe-Client/ReRe-Client/CSceneNode.h
+++ ReRe-Client/ReRe-Client/CSceneNode.h
@@ -2,8 +2,6 @@
 //  SceneNode.h
 //  ReRe-Client
 //
-//  Created by Thomas Kipshagen on 23.04.13.
-//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
 //
 
 #ifndef __ReRe_Client__SceneNode__
