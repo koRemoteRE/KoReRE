380a358a509cbb7160ac7f2421aa439726071078
ThomasKipshagen <tkipshagen@uni-koblenz.de>
2014-07-29 11:20:57 +0200

    Update CPreRendering.cpp

--- ReRe-Client/ReRe-Client/CPreRendering.cpp
+++ ReRe-Client/ReRe-Client/CPreRendering.cpp
@@ -2,8 +2,6 @@
 //  PreRendering.cpp
 //  ReRe-Client
 //
-//  Created by Thomas Kipshagen on 23.04.13.
-//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
 //
 
 #include "CPreRendering.h"
