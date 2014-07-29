448795f9b3aba48dace6407be2ae5d7b3e7adeb0
ThomasKipshagen <tkipshagen@uni-koblenz.de>
2014-07-29 11:21:32 +0200

    Update CSceneManager.cpp

--- ReRe-Client/ReRe-Client/CSceneManager.cpp
+++ ReRe-Client/ReRe-Client/CSceneManager.cpp
@@ -2,8 +2,6 @@
 //  SceneManager.cpp
 //  ReRe-Client
 //
-//  Created by Thomas Kipshagen on 23.04.13.
-//  Copyright (c) 2013 Thomas Kipshagen. All rights reserved.
 //
 // VAO -> Lighthouse3d.com
 
